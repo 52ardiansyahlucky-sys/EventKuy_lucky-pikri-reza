f35bf306bae759ab7f0d15edb4a4c2e4b21678d0
Nama Kamu <email@example.com>
2026-07-10 19:36:57 +0700

    Project selesai

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex items-center justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Daftar Event Saya
+                Daftar Event 
             </h2>
             <!-- CTA dipindahkan ke hero banner -->
         </div>
